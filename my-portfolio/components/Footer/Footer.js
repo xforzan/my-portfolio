25ffc44ed2a8f1c804955789cdc02f5637966ef5
xforzan <nakhila2024@gmail.com>
2024-11-12 00:04:35 +0100

    Update

--- my-portfolio/components/Footer/Footer.js
+++ my-portfolio/components/Footer/Footer.js
@@ -5,9 +5,9 @@ export const Footer = () => `
 
 <h2>Contact</h2>
 <div>
-${Button("/icons/twitter.png", "Twitter")}
-${Button("/icons/github.png", "GitHub")}
-${Button("/icons/linkedin.png", "LinkedIn")}
-${Button("/icons/telegram.png", "Telegram")}
+${Button("/icons/twitter.jpeg", "Twitter")}
+${Button("/icons/github.jpeg", "GitHub")}
+${Button("/icons/linkedin.jpeg", "LinkedIn")}
+${Button("/icons/telegram.jpeg", "Telegram")}
 </div>
 `;
