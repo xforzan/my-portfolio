f26690a1e34043f86dcd4d39a4223d7abd55f16f
xforzan <nakhila2024@gmail.com>
2024-11-11 23:50:01 +0100

    Update

---
+++ my-portfolio/pages/Home.js
@@ -0,0 +1,20 @@
+import "./Home.css";
+import { cleanPage } from "../../utils/cleanPage";
+
+export const Home = () => {
+  const main = document.querySelector("main");
+  cleanPage(main);
+  main.innerHTML = `
+    <section class="home">
+    <p>Hey, I'm</p>
+    <h1>Peter Parker</h1>
+    <p>I'm a software engineer based in Toronto, 
+    Canada and also a communication and journalism student. 
+    I enjoy creating things that live on the internet, 
+    whether that be websites, applications, or anything in between. 
+    I have been freelancing for a year now while studying at the university and 
+    I've manage to gain a decent amount of experience and valuable knowledge 
+    from all different kinds of fields throughout my projects/work.</p>
+    <a href="mailto:peterparker@gmail.com">Say hi →</a>
+    </section>`;
+};
