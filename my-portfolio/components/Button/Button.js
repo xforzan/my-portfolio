31d3137c0d835a1b3018d94aab40cd4587955b8c
xforzan <nakhila2024@gmail.com>
2024-11-12 17:34:03 +0100

    .

--- my-portfolio/components/Button/Button.js
+++ my-portfolio/components/Button/Button.js
@@ -1,8 +1,10 @@
 import "./Button.css";
 
-export const Button = (icon, text) => `
-<button class="my-btn">
+export const Button = (icon, text, link) => `
+<a href=${link} target="_blank">
+    <button class="my-btn">
 <img src=${icon} alt='${text} icon'/>
 <h4>${text}</h4>
 </button>
+</a>
 `;
--- my-portfolio/components/Footer/Footer.js
+++ my-portfolio/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export const Footer = () => `
 <h2>Contact</h2>
 <div>
 ${Button("/icons/x.svg", "X")}
-${Button("/icons/github.jpeg", "GitHub")}
+${Button("/icons/github.jpeg", "GitHub", "https://github.com/xforzan")}
 ${Button("/icons/linkedin.jpeg", "LinkedIn")}
 ${Button("/icons/link.jpeg", "Link")}
 </div>
