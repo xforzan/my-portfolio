d30b9a1b4c13f650ab8feeb40cce127b7e90c700
xforzan <nakhila2024@gmail.com>
2024-11-12 16:22:16 +0100

    Update

--- my-portfolio/data/projects.js
+++ my-portfolio/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
       title: "Proyecto 1",
       description: "Este proyecto consiste en crear una página web responsive, adaptada a todos los dispositivos, utilizando Flexbox y Grid para un diseño flexible y optimizado.",
       image:
-        "https://img.freepik.com/free-photo/smiling-freelancing-developer-home-office-doing-coding-computer_482257-83067.jpg?t=st=1731424245~exp=1731427845~hmac=31a46dfbe0155667e9b98081cf0d1570465e5a351f66a6986d59198001f754ab&w=1380",
+        "./public/img/picture1.png",
       tech: ["HTML5", "CSS3"],
       github: "https://github.com/xforzan/Proyecto_1",
       link: "https://proyecto-1-murex.vercel.app/",
@@ -12,7 +12,7 @@ export const projects = [
       title: "Practica DOM",
       description: "Esta es la descripción de mi segundo proyecto",
       image:
-        "https://images.unsplash.com/photo-1661961111184-11317b40adb2?ixlib=rb-4.0.3&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2344&q=80",
+        "https://img.freepik.com/free-photo/admin-apartment-exasperated-by-annoying-bugs-while-programming_482257-92992.jpg?t=st=1731424561~exp=1731428161~hmac=a78aa9526b3ca5884e2b1bb004ad59c6571114e09add0d1969617ce4a7b5a817&w=1380",
       tech: ["HTML5", "CSS3", "JavaScript"],
       github: "https://github.com/xforzan/Practica-Dom",
       link: "https://practica-dom-nine.vercel.app/",
